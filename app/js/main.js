48e8436052c453f9f8548b147530cabcf84db9dd
ThunderInTheGrass <propeller8924@gmail.com>
2023-12-11 12:36:45 +0800

    строки, колонки, разделители

--- app/js/main.js
+++ app/js/main.js
@@ -8,23 +8,32 @@ const rowInput = document.querySelector('.row-input');
 const columnInput = document.querySelector('.column-input');
 const rowsColumnsBtn = document.querySelector('.rows-columns-btn');
 
+const quantity = document.querySelector('.quantity');
 
-const createTitle = (title, wrapperr) => {
+
+
+const createTitle = (title, wrapperr) => {//генерирует заголовок
   const appTitle = document.createElement("h1");
   appTitle.classList.add("title");
   appTitle.textContent = title;
   wrapperr.append(appTitle);
 };
 
-const createRect = (inner) => {
+const createRect = (inner, elem) => {
+//v1
 //   let newElem = `<svg class="rect" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 100 100">
 //   <rect class="cls-1" x="0.5" y="0.5" width="99" height="99"></rect>
 //   <path class="cls-2" d="M551,86.67v98H453v-98h98m1-1H452v100H552v-100Z" transform="translate(-452 -85.67)"/>
 // </svg>`;
+// inner.innerHTML += newElem;
+
+// v2
   let newElem = document.createElement('div');
   newElem.classList.add('rect');
-  inner.append(newElem)
-  // inner.innerHTML += newElem;
+  inner.append(newElem);
+
+// v3
+// inner.innerHTML += elem;
 };
 
 createRectBtn.addEventListener("click", () => {
@@ -46,19 +55,45 @@ const widthHeightElems = () => {
   })
 }
 
-const createGrid = (rows, columns, wrapper) => {
+const createGrid = (rows, columns, wrapper, quantity) => {
+  let counter = 0;
   for (let i = 0; i < rows.value; i++) {
     let gridRow = document.createElement('div');
     gridRow.classList.add('grid-row')
     wrapper.append(gridRow);
+
     for (let j = 0; j < columns.value; j++) {
       createRect(gridRow);
+
+      counter += 1;
+    }
+  }
+
+  quantity.innerHTML = counter;
+
+  //добавление границ между квадратами 10 на 10
+  const gridRows = document.querySelectorAll('.grid-row');
+  let containerRow;
+
+  for (let i = 0; i < gridRows.length; i++) {
+    containerRow = gridRows[i].querySelectorAll('.rect');
+
+    for (let k = 0; k < containerRow.length; k++) {
+      if ((k + 1) % 10 == 0) {
+        containerRow[k].style.borderRightWidth = '4px';
+      }
+    }
+
+    if ((i + 1) % 10 == 0) {
+      for (let j = 0; j < containerRow.length; j++) {
+          containerRow[j].style.borderBottomWidth = '4px';
+      }
     }
   }
 }
 
 rowsColumnsBtn.addEventListener('click', () => {
-  createGrid(rowInput, columnInput, rectWrapper)
+  createGrid(rowInput, columnInput, rectWrapper, quantity)
 })
 
 
